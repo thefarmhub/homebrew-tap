63ead5cb7c28615d80973d42996c33d77f633bd2
Aquaponics AI <support@aquaponics.ai>
2020-12-13 20:28:18 +0200

    Brew formula update for walle version v0.0.4

--- walle.rb
+++ walle.rb
@@ -2,16 +2,16 @@
 class Walle < Formula
   desc ""
   homepage "https://aquaponics.ai"
-  version "0.0.3"
+  version "0.0.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/aquaponics-ai/walle-dist/releases/download/v0.0.3/walle_0.0.3_mac-os_x86_64.tar.gz"
-    sha256 "3724be0b7d51bb272282caa784adec5cb50bf9c909759dea9db7267fa164b1c8"
+    url "https://github.com/aquaponics-ai/walle-dist/releases/download/v0.0.4/walle_0.0.4_mac-os_x86_64.tar.gz"
+    sha256 "1d135dde934bef61cd0d70ef614de174034c6275f152c22e8340e02168d79775"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/aquaponics-ai/walle-dist/releases/download/v0.0.3/walle_0.0.3_linux_x86_64.tar.gz"
-    sha256 "55bedebfbafae41f2caf46d35e116fe7bfcf4ce6cadc8c735b7f61562f12ef69"
+    url "https://github.com/aquaponics-ai/walle-dist/releases/download/v0.0.4/walle_0.0.4_linux_x86_64.tar.gz"
+    sha256 "df7d9c619b032c0a19d71b6f06ba23ecb80a0130fb029dd86eaa56745cf80f0f"
   end
 
   def install
